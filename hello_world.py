a77ff3a5db580424c67675f3bde5611459ea90c0
shreyas kapse <shreyaskapse171@gmail.com>
2026-05-13 16:18:49 +0530

    add connect to server method

--- hello_world.py
+++ hello_world.py
@@ -9,7 +9,15 @@ def add(a:int, b:int):
 def mul(a:int,b:int)->int:
     return a*b
 
+def connect_to_server(username:str, password:str):
+    print("connection to server by using following username and password")
+    print("username ", username)
+    print("password ",password)
+    
+username = "admin"
+password  = "welcome@123"
 
 greet()
 print(add(10,20))
 print(mul(10,20))
+connect_to_server(username=username, password=password)
