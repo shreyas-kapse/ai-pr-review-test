f74a83e7d1512caaacb34b7d7c32479b3dcf6bdc
shreyas kapse <shreyaskapse171@gmail.com>
2026-05-17 11:52:53 +0530

    add space

--- hello_world.py
+++ hello_world.py
@@ -9,7 +9,7 @@ def add(a:int, b:int):
   
 # multiplication method 
 def mul(a:int,b:int)->int:
-    return a*b 
+    return a*b  
   
 # connect to main server
 def connect_to_server(username:str, password:str):
